2029b4ff802a40faa4d84c927958ff21e2a8ca28
Sam Schantz <35478769+samvschantz@users.noreply.github.com>
2019-01-16 15:42:46 -0800

    Button pushes show up on display

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,21 +4,34 @@ export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = { display: 0 };
+    this.onClick = this.onClick.bind(this);
   }
+
+  onClick(target) {
+    let displayThis = target.toString();
+    this.setState({ display: displayThis });
+  }
+
   render() {
-    return <CalculatorPresenter />;
+    return (
+      <CalculatorPresenter
+        onClick={this.onClick}
+        display={this.state.display}
+      />
+    );
   }
 }
 
 const CalculatorPresenter = props => {
-  const inputNums = [];
-  var num = 0;
-  for (var i = 1; i < 10; i++) {
-    inputNums.push(<button>{i}</button>);
-  }
-  console.log(inputNums);
+  const lessThanTenArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const inputNums = lessThanTenArr.map(num => (
+    <button key={num} onClick={() => props.onClick(num)}>
+      {num}
+    </button>
+  ));
 
-  const display = <div className="display" />;
+  const display = <div className="display">{props.display}</div>;
+
   return (
     <div className="calcBody">
       {display}
