f28373342d047bf48164e6670994420653936b4e
Sam Schantz <35478769+samvschantz@users.noreply.github.com>
2019-01-16 13:02:52 -0800

    Adding initial components
    Trying to make sure state and presentation are managed separately from the start

---
+++ src/Calculator.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Calculator extends Component {
+  constructor(props){
+    super(props);
+    this.state{ display: 0 };
+  }
+  render(){
+    return (
+      <CalculatorPresenter />
+    );
+  }
+}
+
+const CalculatorPresenter = props => {
+  return(<p>Hello</p>);
+}
